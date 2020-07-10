fdd14e85ba4fd77ba07869f3755f897bcaef7756
flippinberger <bergercm@umich.edu>
2020-07-10 17:18:04 -0600

    Client served on backend, and db setup

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,26 +1,60 @@
 import * as cors from "cors";
 import * as express from "express";
 import * as path from "path";
+import * as bodyParser from "body-parser";
+
+import { Sequelize } from "sequelize";
+import { User } from "./models/user";
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 const { PORT = 8080 } = process.env;
 
 // serve static files from the React app
 app.use(express.static(path.join(__dirname, "/usr/client/build")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 // handle api endpoints
-app.get("/", (req: express.Request, res: express.Response) => {
+app.get("/test", (req: express.Request, res: express.Response) => {
   res.send({
     message: "hello world",
   });
 });
 
-if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log("server started at http://localhost:" + PORT);
-  });
-}
+app.get("/users", async (req: express.Request, res: express.Response) => {
+  const users = await User.findAll();
+
+  res.json(users);
+});
+
+app.post("/user", async (req, res) => {
+  console.log("direct access: " + req.body.name);
+  console.log("array access: " + req.body["name"]);
+
+  const name = req.body.name;
+  const user = await User.create({ name: name });
+
+  res.json(user);
+});
+
+var models = require("./models");
+models.sequelize.sync().then(function () {
+  if (require.main === module) {
+    app.listen(PORT, () => {
+      console.log("server started at http://localhost:" + PORT);
+    });
+  }
+});
+
+// if (require.main === module) {
+//   app.listen(PORT, () => {
+//     console.log("server started at http://localhost:" + PORT);
+//   });
+// }
 
 export default app;
