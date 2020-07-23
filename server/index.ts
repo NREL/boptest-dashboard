ca552d086a793e9db50bb9bce1ef48e0ee201963
flippinberger <flippinberger@gmail.com>
2020-07-22 22:21:52 -0600

    No need to serve the front end twice

--- server/index.ts
+++ server/index.ts
@@ -13,10 +13,6 @@ app.use(bodyParser.json());
 // serve static files from the React app
 app.use(express.static(path.join(__dirname, '/usr/client/build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 // define routes
 app.use('/accounts', accountRouter);
 
