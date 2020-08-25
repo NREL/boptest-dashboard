e46a52f3451ec610a67863bb97b1546aeb62916a
flippinberger <flippinberger@gmail.com>
2020-08-25 16:23:21 -0600

    Bold table headers, and code cleanup

--- client/src/Components/ResultsTable.tsx
+++ client/src/Components/ResultsTable.tsx
@@ -11,7 +11,6 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -20,8 +19,6 @@ import Paper from '@material-ui/core/Paper';
 import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
 import DeleteIcon from '@material-ui/icons/Delete';
 import FilterListIcon from '@material-ui/icons/FilterList';
 
@@ -36,32 +33,6 @@ interface Data {
   cost: number; //total operations cost
   emissions: number;
   compTimeRatio: number;
-}
-
-function createData(
-  resultUid: string,
-  testCase: string,
-  dateRun: Date,
-  totalEnergy: number,
-  thermalDiscomfort: number,
-  aqDiscomfort: number,
-  energy: number,
-  cost: number,
-  emissions: number,
-  compTimeRatio: number
-): Data {
-  return {
-    resultUid,
-    testCase,
-    dateRun,
-    totalEnergy,
-    thermalDiscomfort,
-    aqDiscomfort,
-    energy,
-    cost,
-    emissions,
-    compTimeRatio,
-  };
 }
 
 const createDataFromResult = (result): Data => {
@@ -91,22 +62,6 @@ const createRows = (results): Data[] => {
 
   return rows;
 };
-
-// const rows = [
-//   createData('Cupcake', 305, 3.7, 67, 4.3),
-//   createData('Donut', 452, 25.0, 51, 4.9),
-//   createData('Eclair', 262, 16.0, 24, 6.0),
-//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9),
-//   createData('Honeycomb', 408, 3.2, 87, 6.5),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//   createData('Jelly Bean', 375, 0.0, 94, 0.0),
-//   createData('KitKat', 518, 26.0, 65, 7.0),
-//   createData('Lollipop', 392, 0.2, 98, 0.0),
-//   createData('Marshmallow', 318, 0, 81, 2.0),
-//   createData('Nougat', 360, 19.0, 9, 37.0),
-//   createData('Oreo', 437, 18.0, 63, 4.0),
-// ];
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
   if (b[orderBy] < a[orderBy]) {
@@ -248,6 +203,7 @@ function EnhancedTableHead(props: EnhancedTableProps) {
             align={'center'}
             padding={headCell.disablePadding ? 'none' : 'default'}
             sortDirection={orderBy === headCell.id ? order : false}
+            className={classes.headerCell}
           >
             <TableSortLabel
               active={orderBy === headCell.id}
@@ -348,6 +304,9 @@ const useStyles = makeStyles((theme: Theme) =>
     paper: {
       width: '100%',
       marginBottom: theme.spacing(2),
+    },
+    headerCell: {
+      fontWeight: 'bold',
     },
     table: {
       minWidth: 750,
