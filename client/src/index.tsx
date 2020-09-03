9a069cfa780956026c995a9d91b89d0caab6a09d
flippinberger <flippinberger@gmail.com>
2020-09-03 10:21:31 -0600

    Update App to be able to run Modal hooks

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {ModalProvider} from 'react-modal-hook';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ModalProvider>
+      <App />
+    </ModalProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
