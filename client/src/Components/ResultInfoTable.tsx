b26465f021187db4c0308985e7f3d3e09590a1a8
flippinberger <flippinberger@gmail.com>
2020-10-21 17:03:48 -0600

    Long lines in the table scroll horizontally

--- client/src/Components/ResultInfoTable.tsx
+++ client/src/Components/ResultInfoTable.tsx
@@ -1,12 +1,21 @@
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
-import {Table, TableBody, TableCell, TableRow} from '@material-ui/core';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+} from '@material-ui/core';
 import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import {SignatureDetails} from '../../../common/interfaces';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    tableContainer: {
+      padding: '0 0 16px 0',
+    },
     idTable: {
       width: '35%',
     },
@@ -15,6 +24,9 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     grayed: {
       backgroundColor: 'rgb(236, 239, 240)',
+    },
+    longboi: {
+      width: '100%',
     },
   })
 );
@@ -49,152 +61,180 @@ export const ResultInfoTable: React.FC<ResultInfoTableProps> = props => {
 
   return (
     <div className={classes.idTable}>
-      <Typography variant="subtitle2" className={classes.sectionHeader}>
-        BUILDING TYPE
-      </Typography>
-      <Table size="small">
-        <TableBody>
-          <TableRow className={classes.grayed}>
-            <TableCell>
-              <Typography variant="body2">
-                Residential multi-zone hydronic
-              </Typography>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-      <Typography variant="subtitle2" className={classes.sectionHeader}>
-        PARAMETERS
-      </Typography>
-      <Table size="small">
-        <TableBody>
-          <TableRow className={classes.grayed}>
-            <TableCell>
-              <Typography variant="body2">Simulation Time</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="body1">
-                {props.result.simulationTime}
-              </Typography>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <Typography variant="body2">Price Scenario</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="body1">
-                {props.result.priceScenario}
-              </Typography>
-            </TableCell>
-          </TableRow>
-          <TableRow className={classes.grayed}>
-            <TableCell>
-              <Typography variant="body2">Uncertainty Distribution</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="body1">
-                {props.result.uncertaintyDistribution}
-              </Typography>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-      <Typography variant="subtitle2" className={classes.sectionHeader}>
-        GENERAL
-      </Typography>
-      <Table size="small">
-        <TableBody>
-          <TableRow className={classes.grayed}>
-            <TableCell>
-              <Typography variant="body2">Identifier</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="body1">{props.result.resultUid}</Typography>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <Typography variant="body2">Simulation Date</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="body1">{dateString}</Typography>
-            </TableCell>
-          </TableRow>
-          <TableRow className={classes.grayed}>
-            <TableCell>
-              <Typography variant="body2">Contact</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="body1">chris.berger@devetry.com</Typography>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-      {props.result.controllerProperties &&
-        props.result.controllerProperties.length != 0 && (
-          <>
-            <Typography variant="subtitle2" className={classes.sectionHeader}>
-              CONTROLLER PROPERTIES
-            </Typography>
-            <Table size="small">
-              <TableBody>
-                <TableRow className={classes.grayed}>
-                  <TableCell>
-                    <Typography variant="body2">Controller Type</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography variant="body1">
-                      {props.result.controllerProperties.controllerType}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>
-                    <Typography variant="body2">Problem Formulation</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography variant="body1">
-                      {props.result.controllerProperties.problemFormulation}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow className={classes.grayed}>
-                  <TableCell>
-                    <Typography variant="body2">
-                      Controller Model Type
-                    </Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography variant="body1">
-                      {props.result.controllerProperties.modelType}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>
-                    <Typography variant="body2">Number of States</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography variant="body1">
-                      {props.result.controllerProperties.numStates}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow className={classes.grayed}>
-                  <TableCell>
-                    <Typography variant="body2">Prediction Horizon</Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Typography variant="body1">
-                      {props.result.controllerProperties.predictionHorizon}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </>
-        )}
+      <TableContainer className={classes.tableContainer}>
+        <Table size="small">
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <Typography
+                  variant="subtitle2"
+                  className={classes.sectionHeader}
+                >
+                  BUILDING TYPE
+                </Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow className={classes.grayed}>
+              <TableCell className={classes.longboi} colSpan={2}>
+                <Typography variant="body2">
+                  Residential multi-zone hydronic
+                </Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography
+                  variant="subtitle2"
+                  className={classes.sectionHeader}
+                >
+                  PARAMETERS
+                </Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow className={classes.grayed}>
+              <TableCell>
+                <Typography variant="body2">Simulation Time</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="body1">
+                  {props.result.simulationTime}
+                </Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography variant="body2">Price Scenario</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="body1">
+                  {props.result.priceScenario}
+                </Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow className={classes.grayed}>
+              <TableCell>
+                <Typography variant="body2">
+                  Uncertainty Distribution
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="body1">
+                  {props.result.uncertaintyDistribution}
+                </Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography
+                  variant="subtitle2"
+                  className={classes.sectionHeader}
+                >
+                  GENERAL
+                </Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow className={classes.grayed}>
+              <TableCell>
+                <Typography variant="body2">Identifier</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="body1">
+                  {props.result.resultUid}
+                </Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography variant="body2">Simulation Date</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="body1">{dateString}</Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow className={classes.grayed}>
+              <TableCell>
+                <Typography variant="body2">Contact</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="body1">
+                  chris.berger@devetry.comkjdflajsdflahdflajshdfjahsdbfh
+                </Typography>
+              </TableCell>
+            </TableRow>
+            {props.result.controllerProperties &&
+              props.result.controllerProperties.length != 0 && (
+                <>
+                  <TableRow>
+                    <TableCell colSpan={2}>
+                      <Typography
+                        variant="subtitle2"
+                        className={classes.sectionHeader}
+                      >
+                        CONTROLLER PROPERTIES
+                      </Typography>
+                    </TableCell>
+                  </TableRow>
+                  <TableRow className={classes.grayed}>
+                    <TableCell>
+                      <Typography variant="body2">Controller Type</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography variant="body1">
+                        {props.result.controllerProperties.controllerType}
+                      </Typography>
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>
+                      <Typography variant="body2">
+                        Problem Formulation
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography variant="body1">
+                        {props.result.controllerProperties.problemFormulation}
+                      </Typography>
+                    </TableCell>
+                  </TableRow>
+                  <TableRow className={classes.grayed}>
+                    <TableCell>
+                      <Typography variant="body2">
+                        Controller Model Type
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography variant="body1">
+                        {props.result.controllerProperties.modelType}
+                      </Typography>
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>
+                      <Typography variant="body2">Number of States</Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography variant="body1">
+                        {props.result.controllerProperties.numStates}
+                      </Typography>
+                    </TableCell>
+                  </TableRow>
+                  <TableRow className={classes.grayed}>
+                    <TableCell>
+                      <Typography variant="body2">
+                        Prediction Horizon
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography variant="body1">
+                        {props.result.controllerProperties.predictionHorizon}
+                      </Typography>
+                    </TableCell>
+                  </TableRow>
+                </>
+              )}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
