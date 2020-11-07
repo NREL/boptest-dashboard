a8af0334fe3454cfc60c307d15a80c009eb22921
flippinberger <flippinberger@gmail.com>
2020-11-06 17:29:53 -0700

    Never fetch deleted results

--- server/controllers/result.ts
+++ server/controllers/result.ts
@@ -19,6 +19,7 @@ export function getAllSharedResults(): Promise<Result[]> {
   return resultsRepository.find({
     relations: ['account', 'buildingType'],
     where: {
+      deleted: false,
       isShared: true,
     },
   });
@@ -32,6 +33,7 @@ export function getAllResultsForUser(email: string): Promise<Result[]> {
   return repo.find({
     relations: ['account', 'buildingType'],
     where: {
+      deleted: false,
       account: account,
     },
   });
