0821d4e4ab2a1edd860ad68d8ae3bb28c1eba3e9
Philip Gonzalez <pgonzalez115@gmail.com>
2020-11-17 16:22:32 -0700

    Fix app height/width

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -16,8 +16,6 @@ const useStyles = makeStyles((theme: Theme) =>
     pageLayout: {
       display: 'flex',
       flexDirection: 'row',
-      height: '100vh',
-      margin: '-8px',
     },
   })
 );
