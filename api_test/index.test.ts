a555258a73cc568e05cf04ee3f97aa56d0a9e969
Philip Gonzalez <pgonzalez115@gmail.com>
2021-03-18 11:45:00 -0600

    Create session for API tests

--- api_test/index.test.ts
+++ api_test/index.test.ts
@@ -1,28 +1,53 @@
-import axios from "axios";
+const axios = require('axios').default;
+const axiosCookieJarSupport = require('axios-cookiejar-support').default;
+const tough = require('tough-cookie');
+
 import { LoginData, SignupData} from '../common/interfaces';
 
 const authRoute = `http://${process.env.SERVER_HOST}:8080/api/auth`;
 
-const user1Signup: SignupData = {
+const session = axios.create({
+  jar: new tough.CookieJar(),
+  withCredentials: true
+});
+
+axiosCookieJarSupport(session);
+session.defaults.jar = new tough.CookieJar();
+session.defaults.withCredentials = true;
+
+
+const suSignup: SignupData = {
   username: 'user1',
-  email: 'e.mail@mail.com',
+  email: 'email1@email.com',
   password: 'pass'
 };
 
-const user1Login: LoginData = {
-  email: 'e.mail@mail.com',
+const suLogin: LoginData = {
+  email: 'email1@email.com',
+  password: `doesn't matter.`
+};
+
+const nonSuSignup: SignupData = {
+  username: 'user2',
+  email: 'a@email.com',
+  password: 'pass'
+};
+
+const nonSuLogin: LoginData = {
+  email: 'a@email.com',
   password: `doesn't matter.`
 };
 
 const nnonexistentUserLogin: LoginData = {
-  email: 'does.not@exist.com',
+  email: 'doesNot@exist.com',
   password: `doesn't matter.`
 }
 
-describe("Main", () => {
+describe('Main', () => {
   beforeAll((done) => {
-    return axios
-      .post(authRoute + '/signup', user1Signup)
+    return session
+      .post(authRoute + '/signup', suSignup)
+      .then(() => session.post(authRoute + '/signup', nonSuSignup))
       .then(() => done())
       .catch(err => {
         console.log(err);
@@ -30,14 +55,15 @@ describe("Main", () => {
       });
   });
 
-  test("Existing user can be logged in", () => {
-    return axios.post(authRoute + '/login', user1Login).then((res) => {
-      expect(res.status).toEqual(200);
-    });
+  test('Existing user can be logged in', () => {
+    return session.post(authRoute + '/login', suLogin)
+      .then(res => {
+        expect(res.status).toEqual(200);
+      });
   });
 
-  test("Nonexistent user cannot be logged in", () => {
-    return axios.post(authRoute + '/login', nnonexistentUserLogin)
+  test('Nonexistent user cannot be logged in', () => {
+    return session.post(authRoute + '/login', nnonexistentUserLogin)
       .then(() => {
         // We should never hit the happy path.
         expect(false).toEqual(true);
@@ -45,6 +71,21 @@ describe("Main", () => {
       .catch(err => {
         expect(err.response.status).toEqual(500);
         expect(err.response.data.name).toEqual('EntityNotFound');
+      });
+  });
+
+  test('Logged in user can access their information', () => {
+    return session.post(authRoute + '/login', suLogin)
+      .then(res => {
+        expect(res.status).toEqual(200);
+      })
+      .then(() => session.get(authRoute + '/info'))
+      .then(res => {
+        expect(res.data.email).toEqual('email1@email.com');
+      })
+      .catch(err => {
+        // We should never hit the un-happy path.
+        expect(false).toEqual(true);
       });
   });
 });
